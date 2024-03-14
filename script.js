c1d6156efdc8468dc86f07eb80619d9939a210b6
zhangyufeibaobao <zyf040815@qq.com>
2024-03-14 21:04:26 +0800

    3.14练习
    第三周两节课课堂练习

---
+++ script.js
@@ -0,0 +1,9 @@
+function sayHello(){
+  var response = prompt("What is your name?");
+  alert('Hello'+response+',have a nice day!');
+}
+function showTopic(){
+  var x =document.getElementById('demo');
+  x.style.fontSize = '25px';
+  x.style.color ='red';
+}
